4f6aa9af2c06891f65fd487805f2713a4a58e11b
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-09-10 03:16:03 -0300

    [UPDATE] Removing and reordering files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
 
 function component() {
-    var element = document.getElementById('div');
-    element.innerHtml = _.join(['Hello', 'webpack'], ' ');
+    var element = document.createElement('div');
+console.log(element);
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
     return element;
 }
 
